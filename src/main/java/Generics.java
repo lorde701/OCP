36bc3ba25856c73b07ef7c5c4a5e1a38a4082225
Ivanna Stetsyuk <istetsyuk@aplana.com>
2019-01-11 16:54:42 +0300

    коммит

---
+++ src/main/java/Generics.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generics {
+
+    static class Sparrow extends Bird {}
+    static class Bird {}
+
+
+    <Sparrow extends Bird> Sparrow method3(List<Sparrow> list) {
+        return new Sparrow(); // DOES NOT COMPILE
+    }
+
+    public static void main(String[] args) {
+        List<? extends Bird> list = new ArrayList<Bird>();
+        list.add(new Sparrow());
+
+        List<? extends Number> list2 = new ArrayList<Integer>();
+        list2.add(new Integer(5));
+    }
+
+}
